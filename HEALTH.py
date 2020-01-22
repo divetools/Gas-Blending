847965330dd6ab774c1b3cd3cb37bffb56234d61
Tucker <41244877+divetools@users.noreply.github.com>
2020-01-21 20:12:34 -0500

    Update HEALTH.py

--- HEALTH.py
+++ HEALTH.py
@@ -8,10 +8,10 @@ FAC_S1 = (adc(0, gain=16) + adc(0, gain=16) + adc(0, gain=16) + adc(0, gain=16) 
 FAC_S2 = (adc(1, gain=16) + adc(1, gain=16) + adc(1, gain=16) + adc(1, gain=16) + adc(1, gain=16)) / 5 / 20.9
 
 while True:
-   O2_S1 = round(adc.read_adc(0, gain=16) / FAC_S1,1)
-   O2_S2 = round(adc.read_adc(1, gain=16) / FAC_S2,1)
-   S1 = round(adc.read_adc(0, gain=16) * 0.00781274,1)
-   S2 = round(adc.read_adc(0, gain=16) * 0.00781274,1)
+   O2_S1 = round(adc(0, gain=16) / FAC_S1,1)
+   O2_S2 = round(adc(1, gain=16) / FAC_S2,1)
+   S1 = round(adc(0, gain=16) * 0.00781274,1)
+   S2 = round(adc(0, gain=16) * 0.00781274,1)
    os.system('clear')
    print " ", "S1:", O2_S1,'%', "     S2:", O2_S1
    print " "
