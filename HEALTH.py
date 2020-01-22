960a0b5d27b087777c7c45e07634fc9526118464
Tucker <41244877+divetools@users.noreply.github.com>
2020-01-21 21:17:30 -0500

    Update HEALTH.py
    Adding other two sensors

--- HEALTH.py
+++ HEALTH.py
@@ -12,10 +12,16 @@ while True:
    O2_S2 = round(adc(1, gain=16) / FAC_S2,1)
    S1 = round(adc(0, gain=16) * 0.00781274,1)
    S2 = round(adc(1, gain=16) * 0.00781274,1)
+   S3 = round(adc(2, gain=16) * 0.00781274,1)
+   S4 = round(adc(3, gain=16) * 0.00781274,1)
    os.system('clear')
    print " ", "S1:", O2_S1,'%', "     S2:", O2_S2
    print " "
    print " ", "S1:", S1,"mV"
    print " "
    print " ", "S2:", S2,"mV"
+   print " "
+   print " ", "S3:", S3,"mV"
+   print " "
+   print " ", "S4:", S4,"mV"
    time.sleep(0.5)
