93bebed50494ce396388b3de2b5c11e495c29c4f
Tucker <41244877+divetools@users.noreply.github.com>
2020-01-21 20:09:10 -0500

    Update HEALTH.py
    clean up

--- HEALTH.py
+++ HEALTH.py
@@ -2,10 +2,10 @@
 
 import Adafruit_ADS1x15, time, os
 
-adc = Adafruit_ADS1x15.ADS1115()
+adc = Adafruit_ADS1x15.ADS1115.read_adc()
 
-FAC_S1 = (adc.read_adc(0, gain=16) + adc.read_adc(0, gain=16) + adc.read_adc(0, gain=16) + adc.read_adc(0, gain=16) + adc.read_adc(0, gain=16)) / 5 / 20.9 
-FAC_S2 = (adc.read_adc(1, gain=16) + adc.read_adc(1, gain=16) + adc.read_adc(1, gain=16) + adc.read_adc(1, gain=16) + adc.read_adc(1, gain=16)) / 5 / 20.9
+FAC_S1 = (adc(0, gain=16) + adc(0, gain=16) + adc(0, gain=16) + adc(0, gain=16) + adc(0, gain=16)) / 5 / 20.9 
+FAC_S2 = (adc(1, gain=16) + adc(1, gain=16) + adc(1, gain=16) + adc(1, gain=16) + adc(1, gain=16)) / 5 / 20.9
 
 while True:
    O2_S1 = round(adc.read_adc(0, gain=16) / FAC_S1,1)
